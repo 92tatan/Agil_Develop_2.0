5ec187e9016bfffb27add917c74e361491a36a7e
Brian Villada <brian.villada@udea.edu.co>
2021-11-28 15:49:05 -0500

    inscription.ts renamed

---
+++ BACKEND/models/inscription.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const inscriptionSchema = new Schema({
+    id_proyecto:{
+        type: String,               // cambiarlo a tipo proyecto 
+        required: true,
+        //unique: true                // Solo para pruebas    
+    },
+    id_estudiante:{
+        type:String,                // cambiarlo a tipo user
+        required:true
+    },
+    estado:{
+        type:String,
+        required:true
+    },
+    fecha_ingreso:{
+        type:String,
+        required:true
+    },
+    fecha_egreso:{
+        type:String,
+        required:true
+    }
+});
+
+const inscriptionModel =  model('Inscription', inscriptionSchema);
+
+export  {inscriptionModel};

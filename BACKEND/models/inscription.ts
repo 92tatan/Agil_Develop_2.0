e8e990879b3d0d9a1890430b14c8ff5901c10662
Brian Villada <brian.villada@udea.edu.co>
2021-11-28 22:41:14 -0500

    interface typescript added

--- BACKEND/models/inscription.ts
+++ BACKEND/models/inscription.ts
@@ -1,27 +1,39 @@
 import { Schema, model } from "mongoose";
+import { Enum_Estado } from './enums';
 
-const inscriptionSchema = new Schema({
+
+interface Inscription {
+    id_proyecto: string;
+    id_estudiante: string;
+    estado: Enum_Estado;
+    fecha_ingreso: Date
+    fecha_egreso: Date;
+}
+
+const inscriptionSchema = new Schema<Inscription>({
     id_proyecto:{
         type: String,               // cambiarlo a tipo proyecto 
         required: true,
-        //unique: true                // Solo para pruebas    
-    }/* ,
+        //unique: true                  
+    } ,
     id_estudiante:{
         type:String,                // cambiarlo a tipo user
-        required:true
+        required:true, 
+        //unique:true
     },
     estado:{
         type:String,
-        required:true
+        required:true,
+        enum:Enum_Estado
     },
     fecha_ingreso:{
-        type:String,
+        type:Date,
         required:true
     },
     fecha_egreso:{
-        type:String,
+        type:Date,
         required:true
-    } */
+    } 
 });
 
 const inscriptionModel =  model('Inscription', inscriptionSchema);

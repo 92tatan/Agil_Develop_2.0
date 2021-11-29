32f3a15daf3f5653a9e7853c949633b5bf9ec2e3
Brian Villada <brian.villada@udea.edu.co>
2021-11-28 22:40:38 -0500

    user, pass to bd updated

--- BACKEND/index.ts
+++ BACKEND/index.ts
@@ -3,14 +3,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3002;
 import { isConstructorDeclaration } from "typescript";
+import { Enum_Estado } from "./models/enums";
 import { inscriptionModel } from "./models/inscription";
 
 // Conexion a BDs
 const mongoose = require("mongoose");
 
-const user = "admin";
-const password = "Q1w2e3r45t";
-/* const dbname = 'veterinaria'; */
+const user = "userB";
+const password = "accesobd";
+
 const dbname = "Proyecto_test";
 const uri = `mongodb+srv://${user}:${password}@cluster0.isgyv.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 console.log(uri);
@@ -18,6 +19,20 @@ mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(`Bases de datos ${dbname} conectada`))
   .catch((e) => console.log(e));
+
+  const main = async () =>{
+    await inscriptionModel.create({
+      id_proyecto:'7',
+      id_estudiante:'1017125412',
+      fecha_egreso:"01/12/21",
+      fecha_ingreso:"01/01/20",
+      estado:Enum_Estado.aceptada
+    }).then((u)=>{
+    console.log("creado", u)
+    }).catch((e)=>{
+      console.log("error al crear", e)
+    });
+  }
 
   
 
@@ -40,13 +55,6 @@ app.use("/proyectos", require("./router/Proyectos"));
 // Rutas de web para Inscripciones
 app.use("/inscripciones", require("./router/Inscripciones"));
 
-const main = async () =>{
-  inscriptionModel.create({
-    id_proyecto:"2"
-  }).then((u)=>{
-  console.log("creado", u)
-  }).catch((e)=>{
-    console.log("error al crear", e)
-  });
-}
+main ();
+
 

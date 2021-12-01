44402949081d4c9f84f7dff1c5016a1cbf536a83
Brian Villada <brian.villada@udea.edu.co>
2021-12-01 06:21:16 -0500

    apollo server installed used for bd conection

--- BACKEND/index.ts
+++ BACKEND/index.ts
@@ -1,24 +1,41 @@
 "use strict";
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
+
 import { isConstructorDeclaration } from "typescript";
-import { Enum_Estado } from "./models/enums";
+import { Enum_EstadoInscripcion } from "./models/enums";
 import { inscriptionModel } from "./models/inscription";
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import dotenv from 'dotenv'
+import conectarBD from "./db/db"
+import { typeDefs } from "./graphql/types";
+import { resolvers } from "./graphql/resolvers";
 
 // Conexion a BDs
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
-const user = "userB";
-const password = "accesobd";
+//const user = "userB";
+//const password = "accesobd";
 
-const dbname = "Proyecto_test";
-const uri = `mongodb+srv://${user}:${password}@cluster0.isgyv.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-console.log(uri);
-mongoose
+//const dbname = "Proyecto_test";
+//const uri = `mongodb+srv://${user}:${password}@cluster0.isgyv.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+//console.log(uri);
+/*mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(`Bases de datos ${dbname} conectada`))
-  .catch((e) => console.log(e));
+  .catch((e) => console.log(e));*/
+dotenv.config();
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers:resolvers
+  
+
+});
+
+
 
   const main = async () =>{
     await inscriptionModel.create({
@@ -26,7 +43,7 @@ mongoose
       id_estudiante:'1017125412',
       fecha_egreso:"01/12/21",
       fecha_ingreso:"01/01/20",
-      estado:Enum_Estado.aceptada
+      estado:Enum_EstadoInscripcion.aceptada
     }).then((u)=>{
     console.log("creado", u)
     }).catch((e)=>{
@@ -40,9 +57,21 @@ mongoose
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(cors());
+
 // prueba escuchando al puerto
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`API REST corriendo en puerto ${port}`);
+});*/
+
+// 
+app.listen({ port: process.env.PORT || 3002 }, async () => {
+  await conectarBD();
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log('servidor listo');
 });
 
 // Rutas de test
@@ -55,6 +84,6 @@ app.use("/proyectos", require("./router/Proyectos"));
 // Rutas de web para Inscripciones
 app.use("/inscripciones", require("./router/Inscripciones"));
 
-main ();
+//main ();
 
 

11cf2df29bd96a3fbba82ccdc7d4583b520b3021
Brian Villada <brian.villada@udea.edu.co>
2021-12-11 07:30:22 -0500

    tabla estilos

--- FRONTEND/agil_develop_2/src/Components/Inscripciones/ExplorarInscripciones.js
+++ FRONTEND/agil_develop_2/src/Components/Inscripciones/ExplorarInscripciones.js
@@ -9,12 +9,12 @@ export const ExplorarInscripcion = () => {
     console.log("Datos Inscripciones:", data);
   }, [data]);
 
+  if (loading) return <div>Cargando datos...</div>;
+
   return (
     <div>
       <BarNavegador />
-      <h1 className="text-center mt-5">
-        Pantalla de explorar Inscripcion Proyectos:
-      </h1>
+      <h1 className="text-center mt-5">Inscripciones a Proyectos:</h1>
       <table className="tabla">
         <thead>
           <tr>

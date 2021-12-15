b90721f61d986fa42c8f5fff9a20df2f63a08042
Antonia Cardozo <antoniadosdos@gmail.com>
2021-12-14 19:24:02 -0500

    Cambio Archivo perfil

--- FRONTEND/agil_develop_2/src/Components/Usuarios/Perfil.js
+++ FRONTEND/agil_develop_2/src/Components/Usuarios/Perfil.js
@@ -6,7 +6,7 @@ export const Perfil = ()=> {
     return(
         <div>
         <BarNavegador />
-        <h1 className="text-center mt-5" >pantalla de Perfiles </h1>
+        <h1 className="text-center mt-5" >Detalles de Usuario </h1>
         </div>
     )
 
